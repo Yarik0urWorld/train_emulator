39dc9e9bb552a0e049f12cfe32a553681a937b0d
Yarik0urWorld <77680039+Yarik0urWorld@users.noreply.github.com>
2022-01-04 19:30:56 +0300

    Update phys.py to v1.0

--- phys.py
+++ phys.py
@@ -3,7 +3,7 @@ from time import sleep
 
 
 class Train:
-    def __init__(self, max_speed, max_gear, sps, tps=10):
+    def __init__(self, max_speed, max_gear, sps, road_max, road_min=0, tps=10):
         self.current_speed = 0  # Current speed in km/h
         self.max_speed = max_speed  # Maximum speed in km/h
         self.gear = 0
@@ -14,6 +14,9 @@ class Train:
         self.tick_thread = Thread(target=self.tick_thread_method, args=())
         self.running = False
         self.movement_q = 0
+        self.road_min = 0
+        self.road_max = road_max
+        self.road_end_func = None
 
     def __str__(self):
         return f'<Train gear={self.gear} movement_q={self.movement_q}' \
@@ -32,6 +35,14 @@ class Train:
 
         self.pos += self.current_speed / 3.6 / self.tps
 
+        if self.pos < self.road_min:
+            self.pos = self.road_min
+        elif self.pos > self.road_max:
+            self.pos = self.road_max
+
+            if self.road_end_func is not None:
+                self.road_end_func()
+
     def tick_thread_method(self):
         while self.running:
             self.tick()
@@ -47,3 +58,6 @@ class Train:
         from direct.stdpy import thread
 
         thread.start_new_thread(function=self.tick_thread_method, args='')
+
+    def bind_road_end(self, func):
+        self.road_end_func = func 
