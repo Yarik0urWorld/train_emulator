5137dbb56e5515e8c246a95d7063443b342ba8ab
Yaroslav Kustov <77680039+Yarik0urWorld@users.noreply.github.com>
2022-01-06 17:25:13 +0300

    Update phys.py to v1.1.0
    Update v1.1.0 is ready! Uploading all its textures and code!

--- phys.py
+++ phys.py
@@ -1,9 +1,9 @@
 from threading import Thread
 from time import sleep
-
+import math
 
 class Train:
-    def __init__(self, max_speed, max_gear, sps, road_max, road_min=0, tps=10):
+    def __init__(self, max_speed, max_gear, sps, brake_force, brake_secs, nitro_boost, road_max=math.inf, road_min=0, tps=10):
         self.current_speed = 0  # Current speed in km/h
         self.max_speed = max_speed  # Maximum speed in km/h
         self.gear = 0
@@ -17,10 +17,18 @@ class Train:
         self.road_min = 0
         self.road_max = road_max
         self.road_end_func = None
+        self.brake_force = brake_force # km/h per second
+        self.brake_left = 1.0 # 1.0 means that brake is full of energy
+        self.brake_secs = brake_secs
+        self.is_brake_active = False
+        self.nitro_left = 1
+        self.nitro_boost = nitro_boost
 
     def __str__(self):
         return f'<Train gear={self.gear} movement_q={self.movement_q}' \
-               f' current_speed={self.current_speed} pos={self.pos}>'
+               f' current_speed={self.current_speed} pos={self.pos}' \
+               f' brake_force={self.brake_force} brake_left={self.brake_left}' \
+               f' is_brake_active={self.is_brake_active}>'
 
     def tick(self):
         target_speed = self.max_speed / self.max_gear * self.gear * self.movement_q
@@ -35,6 +43,17 @@ class Train:
 
         self.pos += self.current_speed / 3.6 / self.tps
 
+        if self.is_brake_active and self.current_speed > 0 and self.brake_left > 0:
+            self.brake_left -= 1 / self.brake_secs / self.tps
+            if self.brake_left < 0:
+                self.brake_left = 0
+            else:
+                if self.current_speed < self.brake_force:
+                    self.current_speed = 0
+                else:
+                    self.current_speed -= self.brake_force / self.tps
+            #print(f'spent {1 / self.tps} brake, dec. speed by {self.brake_force / self.tps}')
+        
         if self.pos < self.road_min:
             self.pos = self.road_min
         elif self.pos > self.road_max:
@@ -42,7 +61,12 @@ class Train:
 
             if self.road_end_func is not None:
                 self.road_end_func()
-
+                
+    def nitro(self):
+        if self.nitro_left:
+            self.current_speed += self.nitro_boost
+            self.nitro_left = 0
+            
     def tick_thread_method(self):
         while self.running:
             self.tick()
@@ -60,4 +84,4 @@ class Train:
         thread.start_new_thread(function=self.tick_thread_method, args='')
 
     def bind_road_end(self, func):
-        self.road_end_func = func 
+        self.road_end_func = func
